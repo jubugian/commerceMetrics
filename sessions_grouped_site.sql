c049285bb501f7d7f1c898dac137d8c29e092ba9
jubugian <ext_jubugian@mercadolibre.com>
2022-02-21 17:09:07 -0300

    Add files via upload

---
+++ sessions_grouped_site.sql
@@ -0,0 +1,28 @@
+WITH 
+dateFrom AS (SELECT DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) 
+
+, ga_sessions AS (
+
+      SELECT date
+            , site
+            , CONCAT(fullVisitorId,SAFE_CAST(visitStartTime AS STRING)) AS SessionId
+            , fullVisitorId as fullVisitorId
+            , CASE
+                WHEN hits.appInfo.appName IS NOT NULL THEN (totals.hits = 1 AND COALESCE(hits.isExit,FALSE))
+                ELSE NOT (totals.bounces is null)
+                END  AS  isBounce
+            , totals.newVisits AS newVisits
+      FROM `calm-mariner-105612.ALL_GA360.ga_sessions` , unnest(hits) as hits
+      WHERE date = (SELECT * FROM dateFrom)
+      AND businessUnit = 'ML'
+      AND site IN ('MLC','MLU','MPE','MLA','MLM','MLB','MLV','MCO')
+
+)
+SELECT DATE
+     , Site
+     , COUNT(DISTINCT SessionId) AS sessions
+     , COUNT(DISTINCT IF(NOT isBounce, SessionId, NULL)) AS sessionsNotBounce
+     , COUNT(DISTINCT fullVisitorId ) AS Users
+     , COUNT(DISTINCT IF(newVisits=1, fullVisitorId, NULL)) AS newUsers
+FROM ga_sessions
+GROUP BY 1,2
